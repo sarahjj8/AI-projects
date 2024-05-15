055d5f3d79c02ca235ffeeeb89e7399302535787
sarahjj8 <124861791+sarahjj8@users.noreply.github.com>
2024-05-15 20:37:57 +0330

    Add files via upload

---
+++ AI_SP1402_Kirby-master/algorithms/BFSAlgorithm.py
@@ -0,0 +1,58 @@
+from node import Node
+from algorithms.common import find_possible_moves
+from time import process_time
+
+
+class BFS:
+    def __init__(self, world):
+        self.emptyNode = Node(None, None, "first father", -1, 0, 0, 0)
+        self.firstNode = Node(world, self.emptyNode, " ", 0, 0, 0, 0)
+        self.kirbyPos = self.firstNode.searchForKirby()
+        self.stack = [self.firstNode]
+        self.computingTime = ""
+
+    def getComputingTime(self):
+        return self.computingTime
+
+    def setComputingTime(self, computingTime):
+        self.computingTime = computingTime
+
+    def start(self):
+        startTime = process_time()
+        expandedNodes = 0
+        depth = 0
+
+        stack = self.stack
+        currentNode = stack[0]
+        visited = set()  # Initialize an empty set to keep track of visited nodes
+        visited.add(self.firstNode)
+
+        while not currentNode.isGoal():
+            possible_moves = find_possible_moves(currentNode)
+            for move in possible_moves:
+
+                if move not in visited:  # Check if the move has been visited
+                    stack.append(move)
+                    visited.add(move)  # Mark the move as visited
+
+                if move.getDepth() > depth:
+                    depth = move.getDepth()
+
+            stack.remove(currentNode)
+            currentNode = stack[0]
+
+            expandedNodes += 1
+
+        elapsedTime = process_time() - startTime
+        elapsedTimeFormatted = "%.10f s." % elapsedTime
+        self.setComputingTime(elapsedTimeFormatted)
+
+        solution = currentNode.recreateSolutionWorld()
+        solutionWorld = solution[::-1]
+        print("Expanded nodes: ", expandedNodes + 1)  # Good
+        print("Depth: ", depth)
+        print("The final cost of the solution is: " + str(currentNode.getCost()))
+        print(currentNode.recreateSolution())
+        return [solutionWorld, expandedNodes + 1, depth]
+
+
